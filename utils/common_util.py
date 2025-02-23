144ae2350975dbac840652520546c4d8b6b9cb51
NampelliSrilekha <nampelli.srilekha10@gmail.com>
2025-02-23 16:39:27 -0700

    deleted:    config/__init__.py 	deleted:    config/__pycache__/__init__.cpython-311.pyc 	deleted:    config/__pycache__/db_config.cpython-311.pyc 	deleted:    db/__init__.py 	deleted:    db/__pycache__/__init__.cpython-311.pyc 	deleted:    db/__pycache__/db_connection.cpython-311.pyc 	deleted:    models/__pycache__/__init__.cpython-311.pyc 	deleted:    models/__pycache__/membership.cpython-311.pyc 	deleted:    models/__pycache__/users.cpython-311.pyc 	new file:   requirements.txt 	deleted:    services/__pycache__/__init__.cpython-311.pyc 	deleted:    services/__pycache__/membership_services.cpython-311.pyc 	deleted:    services/__pycache__/user_services.cpython-311.pyc 	deleted:    utils/__init__.py 	deleted:    utils/__pycache__/__init__.cpython-311.pyc 	deleted:    utils/__pycache__/json_util.cpython-311.pyc

--- utils/common_util.py
+++
@@ -1,6 +0,0 @@
-# utils/common_utils.py
-import hashlib
-
-def hash_password(password):
-    """Hash password securely"""
-    return hashlib.sha256(password.encode()).hexdigest()
--- utils/file_util.py
+++
@@ -1,6 +0,0 @@
-# utils/file_utils.py
-import os
-
-def file_exists(file_path):
-    """Check if a file exists"""
-    return os.path.exists(file_path)
